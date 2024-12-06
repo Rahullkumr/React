8bea7db54138293d3d22f013059305588849499b
Rahullkumr <kumarrahul771996@gmail.com>
2024-12-07 04:59:01 +0530

    feat: add Layout00 component with associated styles and subcomponents

--- ReactByPrasadSir/src/App.jsx
+++ ReactByPrasadSir/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Layout00 from './components/00_tasks/Layout00.jsx'
 import Layout01 from './components/00_tasks/Layout01.jsx'
 import Layout02 from './components/00_tasks/Layout02.jsx'
 import JsxIntro from './components/01_jsxIntro/JsxIntro.jsx'
@@ -8,7 +9,8 @@ function App() {
   return (
     <>
 {/* ============ Tasks ============ */}
-      <Layout01/>
+      <Layout00/>
+      {/* <Layout01/> */}
       {/* <Layout02/> */}
 
 
