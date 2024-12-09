b769e81cdd4bc5ea160f971ae44740891f21154a
Rahullkumr <kumarrahul771996@gmail.com>
2024-12-10 02:05:19 +0530

    feat: add ClassBasedCompIntro and ClassBasedCompEx1 components with associated styles

--- ReactByPrasadSir/src/App.jsx
+++ ReactByPrasadSir/src/App.jsx
@@ -3,21 +3,25 @@ import Layout00 from './components/00_tasks/Layout00.jsx'
 import Layout01 from './components/00_tasks/Layout01.jsx'
 import Layout02 from './components/00_tasks/Layout02.jsx'
 import JsxIntro from './components/01_jsxIntro/JsxIntro.jsx'
+import ClassBasedCompIntro from './components/02_typesOfComponents/02_classBasedComp/ClassBasedCompIntro.jsx'
 
 function App() {
-
+  
   return (
     <>
 {/* ============ Tasks ============ */}
-      <Layout00/>
-      <Layout01/>
-      <Layout02/>
+      {/* <Layout00/> */}
+      {/* <Layout01/> */}
+      {/* <Layout02/> */}
 
 
 
 {/* ============ Components ============ */}
-      <JsxIntro/>
-      
+      {/* <JsxIntro/> */}
+
+{/* ============ types of Components ============ */}
+      <ClassBasedCompIntro/>
+
     </>
   )
 }
