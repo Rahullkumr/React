18225f33a3db3c21da6b4b68f996de78d90231c2
Rahullkumr <kumarrahul771996@gmail.com>
2024-12-13 03:07:20 +0530

    feat: add day12 - PropsIntro component and examples for props (needs attention)

--- ReactByPrasadSir/src/App.jsx
+++ ReactByPrasadSir/src/App.jsx
@@ -5,26 +5,29 @@ import Layout02 from './components/00_tasks/Layout02.jsx'
 import JsxIntro from './components/01_jsxIntro/JsxIntro.jsx'
 import ClassBasedCompIntro from './components/02_typesOfComponents/02_classBasedComp/ClassBasedCompIntro.jsx'
 import HooksIntro from './components/03_hooks/HooksIntro.jsx'
+import PropsIntro from './components/04_props/PropsIntro.jsx'
 
 function App() {
   
   return (
     <>
-{/* ============ Tasks ============ */}
-      {/* <Layout00/> */}
-      {/* <Layout01/> */}
-      {/* <Layout02/> */}
+      {/* ============ Tasks ============ */}
+            {/* <Layout00/> */}
+            {/* <Layout01/> */}
+            {/* <Layout02/> */}
 
 
 
-{/* ============ Components and its types ============ */}
-      {/* <JsxIntro/> */}
-      {/* <ClassBasedCompIntro/> */}
+      {/* ============ Components and its types ============ */}
+            {/* <JsxIntro/> */}
+            {/* <ClassBasedCompIntro/> */}
 
 
-{/* ============ hooks ============ */}
-      <HooksIntro/>
+      {/* ============ hooks ============ */}
+            {/* <HooksIntro/> */}
 
+      {/* ============ hooks ============ */}
+            <PropsIntro/>
     </>
   )
 }
